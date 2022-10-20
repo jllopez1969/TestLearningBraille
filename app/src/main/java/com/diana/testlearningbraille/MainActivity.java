238700eb6355549a8a5283b76f977c9d00ec46dd
jllopez1969 <49125238+jllopez1969@users.noreply.github.com>
2022-10-20 17:34:44 +0200

    Correcciones propuestas realizadas

--- app/src/main/java/com/diana/testlearningbraille/MainActivity.java
+++ app/src/main/java/com/diana/testlearningbraille/MainActivity.java
@@ -1,6 +1,7 @@
 package com.diana.testlearningbraille;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
@@ -23,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_main);
 
 
